9b417c6fdfa2dfbea92080b50272335268dee58a
Muhammad Azhar Mujahid <azhar@DESKTOP-FOJ1FOE>
2019-12-03 04:05:58 +0700

    upload file

---
+++ BAB_1/FileJs.js
@@ -0,0 +1,11 @@
+/* Berikut diberikan contoh Program Javascript yang diletakkan di file lain dan dipanggil melalui
+suatu file HTML
+*/
+
+document.writeln("<center>");
+document.writeln("<hr width=600 color=black>");
+document.writeln("<h1>UNIVERSITAS INDONESIA</h1>");
+document.writeln("<h2>FAKULTAS TEKNIK dan ILMU KOMPUTER</h2>");
+document.writeln("<h3>JURUSAN TEKNIK INFORMATIKA</h3>");
+document.writeln("<HR WIDTH=600 COLOR=black>");
+document.writeln("</CENTER>");
---
+++ BAB_1/Pendahuluan.js
@@ -0,0 +1,18 @@
+/*
+Javascript diperkenalkan pertama kali oleh Netscape pada tahun 1995. Pada awalnya bahasa ini
+dinamakan “LiveScript” yang berfungsi sebagai bahasa sederhana untuk browser Netscape
+Navigator 2. 
+
+Javascript bergantung kepada browser(navigator) yang memanggil halaman web yang berisi skrip
+skrip dari Javascript dan tentu saja terselip di dalam dokumen HTML. Javascript juga tidak
+memerlukan kompilator atau penterjemah khusus untuk menjalankannya (pada kenyataannya
+kompilator Javascript sendiri sudah termasuk di dalam browser tersebut). Lain halnya dengan
+bahasa “Java” (dengan mana JavaScript selalu di banding bandingkan) yang memerlukan
+kompilator khusus untuk menterjemahkannya di sisi user/klien. 
+
+Java script ditulis pada file yang sama
+Untuk penulisan dengan cara ini, perintah yang digunakan adalah
+<SCRIPT LANGUANGE =”JavaScript” >program java script disini</SCRIPT>. Perintah tersebut
+biasanya diletakkan diantara Tag <BODY>…</BODY>
+
+*/ 
